c627e3917cd5bdbd2eefb3f1ee52bc07bff3f33a
Joel <joel.m.maenpaa@gmail.com>
2020-03-24 19:45:13 +0200

    get evolution line function

--- helpers/pokeapi/poke.js
+++ helpers/pokeapi/poke.js
@@ -7,5 +7,9 @@ module.exports = {
             .then(res => cb(res.data))
             .catch(err => console.log(err))
     },
-    
+    getEvolutionLine: (name, cb) => {
+        axios.get(`${base}/evolution-chain/${name}`)
+            .then(res => cb(res.data))
+            .catch(err => console.log(err))
+    }
 }
