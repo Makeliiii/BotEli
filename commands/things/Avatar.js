60143c78c50c10a92d6777f69cd01fe9f4ca4ec1
Joel <joel.m.maenpaa@gmail.com>
2020-03-25 18:57:03 +0200

    avatar error handling

---
+++ commands/things/Avatar.js
@@ -0,0 +1,32 @@
+const { Command } = require('discord-akairo')
+
+module.exports = class AvatarCommand extends Command {
+    constructor() {
+        super('avatar', {
+            aliases: ['avatar'],
+            args: [
+                {
+                    id: 'size',
+                    type: 'number',
+                    default: 128
+                }
+            ]
+        })
+    }
+
+    exec(msg, args) {
+        if (args.size % 16 != 0 || args.size > 2049) {
+            return msg.reply(`that's not a valid image size.`)
+        }
+
+        let avatarOPTS = {
+            format: 'png',
+            dynamic: true,
+            size: args.size
+        }
+
+        let avatar = msg.author.displayAvatarURL(avatarOPTS)
+
+        return msg.reply(`your avatar ${avatar}`)
+    }
+}
