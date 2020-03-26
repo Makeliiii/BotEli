289022a28670204dca541d29dafc49fb5cdc03e3
Joel <joel.m.maenpaa@gmail.com>
2020-03-26 13:16:44 +0200

    better formatting for pokemoninfo

--- commands/pokemon/PokemonInfo.js
+++ commands/pokemon/PokemonInfo.js
@@ -1,5 +1,5 @@
 const { Command } = require('discord-akairo')
-const { getPokemon } = require('../../helpers/pokeapi/poke')
+const { getPokemon } = require('../../helpers/pokeapi/pokemon')
 
 module.exports = class PokemonInfo extends Command {
     constructor() {
@@ -32,15 +32,23 @@ module.exports = class PokemonInfo extends Command {
             const embed = {
                 title: capitalize(name),
                 url: `https://bulbapedia.bulbagarden.net/wiki/${capitalize(name)}`,
-                author: {
-                    name: 'Leninardo',
-                    icon_url: 'https://cdn.discordapp.com/app-icons/685546965618130997/5f9a74df2972c86ddb3c805e4e578fca.png',
-                    url: 'https://github.com/Makeliiii/Leninardo'
-                },
                 fields: [
                     {
                         name: '**ID**',
                         value: id,
+                        inline: true
+                    },
+                    {
+                        name: '**Types**',
+                        ...(types.length === 2 ? { value: `${capitalize(types[0].type.name)},\n${capitalize(types[1].type.name)}` }
+                                               : { value: `${capitalize(types[0].type.name)}` }),
+                        inline: true
+                    },
+                    {
+                        name: '**Ability**',
+                        ...(abilities.length === 3 ? { value: `${capitalize(abilities[0].ability.name)} (Hidden),\n${capitalize(abilities[1].ability.name)},\n${capitalize(abilities[2].ability.name)}` }
+                                                   : abilities.length === 2 ? { value: `${capitalize(abilities[0].ability.name)} (Hidden),\n${capitalize(abilities[1].ability.name)}` }
+                                                                            : { value: `${capitalize(abilities[0].ability.name)}` }),
                         inline: true
                     },
                     {
@@ -54,56 +62,18 @@ module.exports = class PokemonInfo extends Command {
                         inline: true
                     },
                     {
-                        name: '**Types**',
-                        ...(types.length === 2 ? { value: `${capitalize(types[0].type.name)}/${capitalize(types[1].type.name)}` }
-                                               : { value: `${capitalize(types[0].type.name)}` }),
-                        inline: true
-                    },
-                    {
-                        name: '**Ability**',
-                        ...(abilities.length === 3 ? { value: `${capitalize(abilities[0].ability.name)} (Hidden)/${capitalize(abilities[1].ability.name)}/${capitalize(abilities[2].ability.name)}` }
-                                                   : abilities.length === 2 ? { value: `${capitalize(abilities[0].ability.name)} (Hidden)/${capitalize(abilities[1].ability.name)}` }
-                                                   : { value: `${capitalize(abilities[0].ability.name)}` }),
-                        inline: true
-                    },
-                    {
                         name: '**Base XP**',
                         value: `${base_experience}`,
                         inline: true
                     },
-                    {
-                        name: '**Stats**',
-                        value: `https://bulbapedia.bulbagarden.net/wiki/${capitalize(name)}#Stats`,
-                        inline: true
-                    },
-                    /*{
-                        name: '\u200B',
-                        value: `\u200B`
-                    },
-                    {
-                        name: 'Stats',
-                        value: `${capitalize(stats[0].stat.name)}
-                                ${capitalize(stats[1].stat.name)}
-                                ${capitalize(stats[2].stat.name)}
-                                ${capitalize(stats[3].stat.name)}
-                                ${capitalize(stats[4].stat.name)}
-                                ${capitalize(stats[5].stat.name)}`,
-                        inline: true
-                    },
-                    {
-                        name: 'Value',
-                        value: `${stats[0].base_stat}
-                                ${stats[1].base_stat}
-                                ${stats[2].base_stat}
-                                ${stats[3].base_stat}
-                                ${stats[4].base_stat}
-                                ${stats[5].base_stat}`,
-                        inline: true
-                    },*/
                 ],
                 thumbnail: {
                     url: sprites.front_default
-                }
+                },
+                timestamp: new Date(),
+                footer: {
+                    text: 'Leninardo',
+                },
             }
 
             return msg.channel.send({ embed })
