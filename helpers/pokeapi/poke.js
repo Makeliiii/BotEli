5eeffc79814954ff51cd19b82fef2978cb088dd2
Joel <joel.m.maenpaa@gmail.com>
2020-03-26 12:54:44 +0200

    return something better from catch

--- helpers/pokeapi/poke.js
+++ helpers/pokeapi/poke.js
@@ -5,7 +5,10 @@ module.exports = {
     getPokemon: (name, cb) => {
         axios.get(`${base}/pokemon/${name}`)
             .then(res => cb(res.data))
-            .catch(err => console.log(err))
+            .catch(err => {
+                console.log(err.request.connection._httpMessage.res.statusCode)
+                return cb(err.request.connection._httpMessage.res)
+            })
     },
     getEvolutionLine: (id, cb) => {
         axios.get(`${base}/evolution-chain/${id}`)
