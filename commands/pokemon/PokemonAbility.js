3f2b29f9c4bd9dba9e0952d47131773e1b45f1f2
Joel <joel.m.maenpaa@gmail.com>
2020-03-28 00:33:39 +0200

    pokemon ability command

---
+++ commands/pokemon/PokemonAbility.js
@@ -0,0 +1,55 @@
+const { Command } = require('discord-akairo')
+const { getPokeAPI } = require('../../utils/pokeAPI')
+const { capitalize } = require('../../utils/tools')
+
+module.exports = class PokemonAbility extends Command {
+    constructor() {
+        super('pokemonability', {
+            aliases: ['pokemonability', 'ability'],
+            cooldown: 10000,
+            ratelimit: 1,
+            args: [
+                {
+                    id: 'name',
+                    type: 'lowercase'
+                }
+            ]
+        })
+    }
+
+    exec(msg, args) {
+        if (args.name === null) {
+            return msg.channel.send('Please provide correct arguments!')
+        }
+
+        const id = args.name
+
+        getPokeAPI(id, 'ability', (res) => {
+            const { id, name, generation, effect_entries, flavor_text_entries, statusCode } = res
+
+            if (statusCode === 404) {
+                return msg.channel.send('Ability not found')
+            }
+
+            const embed = {
+                title: capitalize(name),
+                fields: [
+                    {
+                        name: '**ID**',
+                        value: id
+                    },
+                    {
+                        name: '**Description**',
+                        value: flavor_text_entries[2].flavor_text
+                    },
+                    {
+                        name: '**Effect**',
+                        ...(effect_entries[0].effect.includes('$effect_chance%') ? { value: effect_entries[0].effect.replace(/\$effect_chance/gi, effect_chance) } : { value: effect_entries[0].effect })
+                    }
+                ]
+            }
+
+            return msg.channel.send({ embed })
+        })
+    }
+}
