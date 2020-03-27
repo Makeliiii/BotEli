33d25b65e947e9c6f79ba86985c034da1f14577b
Joel <joel.m.maenpaa@gmail.com>
2020-03-27 21:33:37 +0200

    new tool

--- helpers/tools/tools.js
+++ helpers/tools/tools.js
@@ -1,5 +1,17 @@
 module.exports = {
     capitalize: string => {
         return string.charAt(0).toUpperCase() + string.slice(1)
+    },
+    capitalizeWords: string => {
+        const splitString = string.split('-')
+        let arr = []
+        let newString
+
+        for (let word of splitString) {
+            arr.push(word.charAt(0).toUpperCase() + word.slice(1))
+        }
+
+        newString = arr.join(' ')
+        return newString
     }
 }
