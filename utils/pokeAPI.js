18f12fc45061240d9eff82cad6faf03b1955bd61
Joel <joel.m.maenpaa@gmail.com>
2020-03-28 00:20:37 +0200

    reduced pokeapi util to one function

--- utils/pokeAPI.js
+++ utils/pokeAPI.js
@@ -2,20 +2,12 @@ const axios = require('axios').default
 const base = 'https://pokeapi.co/api/v2'
 
 module.exports = {
-    getPokemon: (name, cb) => {
-        axios.get(`${base}/pokemon/${name}`)
+    getPokeAPI: (name, endPoint, cb) => {
+        axios.get(`${base}/${endPoint}/${name}`)
             .then(res => cb(res.data))
             .catch(err => {
-                console.log(err.request.connection._httpMessage.res.statusCode)
+                console.log(err)
                 return cb(err.request.connection._httpMessage.res)
             })
     },
-    getMove: (id, cb) => {
-        axios.get(`${base}/move/${id}`)
-            .then(res => cb(res.data))
-            .catch(err => {
-                console.log(err.request.connection._httpMessage.res.statusCode)
-                return cb(err.request.connection._httpMessage.res)
-            })
-    }
 }
