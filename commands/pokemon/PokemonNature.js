35d46ebb60da5d85eebd68b1542c255af6871cbc
Joel <joel.m.maenpaa@gmail.com>
2020-03-28 10:34:50 +0200

    pokemon nature command

---
+++ commands/pokemon/PokemonNature.js
@@ -0,0 +1,59 @@
+const { Command } = require('discord-akairo')
+const { getPokeAPI } = require('../../utils/pokeAPI')
+const { capitalize } = require('../../utils/tools')
+
+module.exports = class PokemonNature extends Command {
+    constructor() {
+        super('pokemonnature', {
+            aliases: ['pokemonnature', 'nature'],
+            cooldown: 10000,
+            ratelimit: 1,
+            args: [
+                {
+                    id: 'name',
+                    type: 'lowercase',
+                }
+            ]
+        })
+    }
+
+    exec(msg, args) {
+        if (args.name === null) {
+            return msg.channel.send('Please provide correct arguments!')
+        }
+
+        console.log(args.name)
+
+        getPokeAPI(args.name, 'nature', (res) => {
+            const { name, id, increased_stat, decreased_stat, likes_flavor, hates_flavor } = res
+
+            const embed = {
+                title: capitalize(name),
+                fields: [
+                    {
+                        name: '**ID**',
+                        value: id,
+                    },
+                    {
+                        name: '**Increased Stat**',
+                        ...(increased_stat === null ? { value: 'None' } : { value: capitalize(increased_stat.name) }),
+                    },
+                    {
+                        name: '**Decreased Stat**',
+                        ...(decreased_stat === null ? { value: 'None' } : { value: capitalize(decreased_stat.name) }),
+                    },
+                    {
+                        name: '**Likes Flavor**',
+                        ...(likes_flavor === null ? { value: 'None' } : { value: capitalize(likes_flavor.name) }),
+                    },
+                    {
+                        name: '**Hates Flavor**',
+                        ...(hates_flavor === null ? { value: 'None' } : { value: capitalize(hates_flavor.name) }),
+                    },
+                ]
+            }
+
+            return msg.channel.send({ embed })
+        })
+    }
+}
