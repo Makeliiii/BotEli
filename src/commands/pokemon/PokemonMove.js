b06daf6e715db70ef08613a37ecb1aac66803acf
Joel <joel.m.maenpaa@gmail.com>
2020-04-19 01:15:43 +0300

    move to promise

--- src/commands/pokemon/PokemonMove.js
+++ src/commands/pokemon/PokemonMove.js
@@ -27,37 +27,37 @@ export default class PokemonMoves extends Command {
         }
 
         const id = args.name.replace(/ /gi, '-')
-        
-        getPokeAPI(id, 'move', (res) => {
-            const { id, name, accuracy, power, pp, stat_changes, effect_entries, effect_chance, flavor_text_entries, type, statusCode } = res
 
-            if (statusCode === 404) {
-                return msg.channel.send('Move not found!')
-            }
+        getPokeAPI(id, 'move')
+            .then(move => {
+                const { id, name, accuracy, power, pp, stat_changes, effect_entries, effect_chance, flavor_text_entries, type } = move
 
-            const embed = new MessageEmbed()
-                .setTitle(capitalizeWords(name))
-                .addField('**Description**', flavor_text_entries[2].flavor_text)
-                .addField(
-                    '**Effect**',
-                    effect_entries[0].effect.includes('$effect_chance%') ? effect_entries[0].effect.replace(/\$effect_chance/gi, effect_chance) : effect_entries[0].effect,
-                )
-                .addField('**ID**', id, true)
-                .addField('**Accuracy**', !accuracy ? '—' : accuracy, true)
-                .addField('**Power**', !power ? '—' : power, true)
-                .addField('**PP**', pp, true)
-                .addField('**Type**', capitalize(type.name), true)
-                .addField(
-                    '**Stat Changes**',
-                    !Array.isArray(stat_changes) || !stat_changes.length ? 'None' : stat_changes.map(stat => {
-                        return `${stat.change} ${capitalize(stat.stat.name)}`
-                    }),
-                    true
-                )
-                .setTimestamp(new Date())
-                .setFooter('Leninardo')
+                const embed = new MessageEmbed()
+                    .setTitle(capitalizeWords(name))
+                    .addField('**Description**', flavor_text_entries[2].flavor_text)
+                    .addField(
+                        '**Effect**',
+                        effect_entries[0].effect.includes('$effect_chance%') ? effect_entries[0].effect.replace(/\$effect_chance/gi, effect_chance) : effect_entries[0].effect,
+                    )
+                    .addField('**ID**', id, true)
+                    .addField('**Accuracy**', !accuracy ? '—' : accuracy, true)
+                    .addField('**Power**', !power ? '—' : power, true)
+                    .addField('**PP**', pp, true)
+                    .addField('**Type**', capitalize(type.name), true)
+                    .addField(
+                        '**Stat Changes**',
+                        !Array.isArray(stat_changes) || !stat_changes.length ? 'None' : stat_changes.map(stat => {
+                            return `${stat.change} ${capitalize(stat.stat.name)}`
+                        }),
+                        true
+                    )
+                    .setTimestamp(new Date())
+                    .setFooter('Leninardo')
 
-            return msg.channel.send(embed)
-        })
+                return msg.channel.send(embed)
+            })
+            .catch(err => {
+                return msg.channel.send(`Server responded with a status code of ${err.statusCode} and with message: ${err.statusMessage}`)
+            })
     }
 }
