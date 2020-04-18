31ed5437fa9ce5fc07eaa99fde89cc5d29ff288e
Joel <joel.m.maenpaa@gmail.com>
2020-04-19 01:15:33 +0300

    ability to promise

--- src/commands/pokemon/PokemonAbility.js
+++ src/commands/pokemon/PokemonAbility.js
@@ -25,25 +25,25 @@ export default class PokemonAbility extends Command {
             return msg.channel.send('Please provide correct arguments!')
         }
 
-        getPokeAPI(args.name, 'ability', (res) => {
-            const { id, name, effect_entries, flavor_text_entries, statusCode } = res
-
-            if (statusCode === 404) {
-                return msg.channel.send('Ability not found')
-            }
-
-            const embed = new MessageEmbed()
-                .setTitle(capitalize(name))
-                .addField('**ID**', id)
-                .addField('**Description**', flavor_text_entries[2].flavor_text)
-                .addField(
-                    '**Effect**',
-                    effect_entries[0].effect.includes('$effect_chance%') ? effect_entries[0].effect.replace(/\$effect_chance/gi, effect_chance) : effect_entries[0].effect
-                )
-                .setTimestamp(new Date())
-                .setFooter('Leninardo')
-
-            return msg.channel.send(embed)
-        })
+        getPokeAPI(args.name, 'ability')
+            .then(ability => {
+                const { id, name, effect_entries, flavor_text_entries } = ability
+    
+                const embed = new MessageEmbed()
+                    .setTitle(capitalize(name))
+                    .addField('**ID**', id)
+                    .addField('**Description**', flavor_text_entries[2].flavor_text)
+                    .addField(
+                        '**Effect**',
+                        effect_entries[0].effect.includes('$effect_chance%') ? effect_entries[0].effect.replace(/\$effect_chance/gi, effect_chance) : effect_entries[0].effect
+                    )
+                    .setTimestamp(new Date())
+                    .setFooter('Leninardo')
+    
+                return msg.channel.send(embed)
+            })
+            .catch(err => {
+                return msg.channel.send(`Server responded with a status code of ${err.statusCode} and with message: ${err.statusMessage}`)
+            })
     }
 }
