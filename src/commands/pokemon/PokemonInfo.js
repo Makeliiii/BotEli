cb551a361ad6d9b248f0db3c229497c66eae7267
Joel <joel.m.maenpaa@gmail.com>
2020-04-19 01:15:38 +0300

    info to promise

--- src/commands/pokemon/PokemonInfo.js
+++ src/commands/pokemon/PokemonInfo.js
@@ -24,43 +24,43 @@ export default class PokemonInfo extends Command {
         if (!args.name) {
             return msg.channel.send('Please provide correct arguments!')
         }
-        
-        getPokeAPI(args.name, 'pokemon', (res) => {
-            const { name, id, height, weight, types, sprites, base_experience, abilities, statusCode } = res
 
-            if (statusCode === 404) {
-                return msg.channel.send('Pokemon not found!')
-            }
+        getPokeAPI(args.name, 'pokemon')
+            .then(pokemon => {
+                const { name, id, height, weight, types, sprites, base_experience, abilities } = pokemon
 
-            const embed = new MessageEmbed()
-                .setTitle(capitalize(name))
-                .setURL(`https://bulbapedia.bulbagarden.net/wiki/${capitalize(name)}`)
-                .setThumbnail(sprites.front_default)
-                .addField('**ID**', id, true)
-                .addField(
-                    '**Types**',
-                    types.map(type => {
-                        return capitalize(type.type.name)
-                    }),
-                    true
-                )
-                .addField(
-                    '**Abilities**',
-                    abilities.map(ability => {
-                        if (ability.is_hidden) {
-                            return `${capitalize(ability.ability.name)} (Hidden)`
-                        }
-                        return capitalize(ability.ability.name)
-                    }),
-                    true
-                )
-                .addField('**Height**', `${height / 10}m`, true)
-                .addField('**Weight**', `${weight / 10}kg`, true)
-                .addField('**Base XP**', base_experience, true)
-                .setTimestamp(new Date())
-                .setFooter('Leninardo')
+                const embed = new MessageEmbed()
+                    .setTitle(capitalize(name))
+                    .setURL(`https://bulbapedia.bulbagarden.net/wiki/${capitalize(name)}`)
+                    .setThumbnail(sprites.front_default)
+                    .addField('**ID**', id, true)
+                    .addField(
+                        '**Types**',
+                        types.map(type => {
+                            return capitalize(type.type.name)
+                        }),
+                        true
+                    )
+                    .addField(
+                        '**Abilities**',
+                        abilities.map(ability => {
+                            if (ability.is_hidden) {
+                                return `${capitalize(ability.ability.name)} (Hidden)`
+                            }
+                            return capitalize(ability.ability.name)
+                        }),
+                        true
+                    )
+                    .addField('**Height**', `${height / 10}m`, true)
+                    .addField('**Weight**', `${weight / 10}kg`, true)
+                    .addField('**Base XP**', base_experience, true)
+                    .setTimestamp(new Date())
+                    .setFooter('Leninardo')
 
-            return msg.channel.send(embed)
-        })
+                return msg.channel.send(embed)
+            })
+            .catch(err => {
+                return msg.channel.send(`Server responded with a status code of ${err.statusCode} and with message: ${err.statusMessage}`)
+            })
     }
 }
