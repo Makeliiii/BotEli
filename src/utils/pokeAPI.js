327a7e5d35bf71689cab5e39d4ba941719273d3c
Joel <joel.m.maenpaa@gmail.com>
2020-04-19 01:10:18 +0300

    promises are cool

--- src/utils/pokeAPI.js
+++ src/utils/pokeAPI.js
@@ -2,10 +2,15 @@ import axios from 'axios'
 const base = 'https://pokeapi.co/api/v2'
 
 export const getPokeAPI = (name, endPoint, cb) => {
-    axios.get(`${base}/${endPoint}/${name}`)
-        .then(res => cb(res.data))
-        .catch(err => {
-            console.log(err)
-            return cb(err.request.connection._httpMessage.res)
+    if (cb) {
+        axios.get(`${base}/${endPoint}/${name}`)
+            .then(res => cb(res.data))
+            .catch(err => cb(err.request.connection._httpMessage.res))
+    }
+
+    return new Promise((resolve, reject) => {
+        axios.get(`${base}/${endPoint}/${name}`)
+            .then(res => resolve(res.data))
+            .catch(err => reject(err.request.connection._httpMessage.res))
     })
 }
