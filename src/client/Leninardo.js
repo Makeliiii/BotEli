993502940291274569e11c14cb67f1ef331a099a
Joel <joel.m.maenpaa@gmail.com>
2020-04-26 16:15:51 +0300

    separated client and index.js

---
+++ src/client/Leninardo.js
@@ -0,0 +1,29 @@
+import { AkairoClient, CommandHandler, ListenerHandler } from 'discord-akairo'
+import { join } from 'path'
+
+export default class LeninardoClient extends AkairoClient {
+    constructor(ownerID, token) {
+        super({ ownerID })
+        this.token = token
+    }
+
+    commandHandler = new CommandHandler(this, {
+        directory: join(__dirname, '..', 'commands'),
+        prefix: '!',
+        defaultCooldown: 1000
+    })
+
+    listenerHandler = new ListenerHandler(this, { directory: join(__dirname, '..', 'listeners') })
+
+    init() {
+        this.commandHandler.useListenerHandler(this.listenerHandler)
+
+        this.commandHandler.loadAll()
+        this.listenerHandler.loadAll()
+    }
+
+    start() {
+        this.init()
+        return this.login(this.token)
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { AkairoClient, CommandHandler, ListenerHandler } from 'discord-akairo'
+import LeninardoClient from './client/Leninardo'
 import { Player } from 'discord-player'
 import dotenv from 'dotenv'
 
@@ -7,30 +7,8 @@ const ownerID = process.env.ownerID
 const token = process.env.TOKEN
 const youtube = process.env.youtube
 
-class Client extends AkairoClient {
-    constructor() {
-        super({
-            ownerID: ownerID
-        })
+const client = new LeninardoClient(ownerID, token)
 
-        this.commandHandler = new CommandHandler(this, {
-            directory: __dirname + '/commands/',
-            prefix: '!'
-        })
+export const player = new Player(client, youtube)
 
-        this.listenerHandler = new ListenerHandler(this, {
-            directory: __dirname + '/listeners/'
-        })
-
-        this.commandHandler.loadAll()
-
-        this.commandHandler.useListenerHandler(this.listenerHandler)
-        this.listenerHandler.loadAll()
-    }
-}
-
-const client = new Client()
-const player = new Player(client, youtube)
-client.player = player
-
-client.login(token)
+client.start()
