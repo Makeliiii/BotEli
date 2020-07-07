660224bd7dda591ce6fcc8641a667023ebb66354
Joel <joel.m.maenpaa@gmail.com>
2020-07-08 01:28:50 +0300

    play command supports yt links now

--- src/commands/music/Play.js
+++ src/commands/music/Play.js
@@ -20,25 +20,28 @@ export default class Play extends Command {
     }
 
     async exec(msg, args) {
+        const { channel } = msg.member.voice
+
         if (!args.url) {
             return msg.channel.send('Please provide an argument!')
         }
 
-        if (!msg.member.voice.channel) {
+        if (!channel) {
             return msg.channel.send('You must be in a voice channel to play music!')
         }
         
         await search(args.url)
-            .then(video => {
+            .then(async video => {
                 const url = `https://www.youtube.com/watch?v=${video.id}`
 
                 const serverQue = msg.client.queue.get(msg.guild.id)
-                const songInfo = ytdl.getInfo(url, '$1')
                 const song = {
-                    id: songInfo.video_id,
-                    title: songInfo.title,
-                    url: songInfo.video_url
+                    id: video.id,
+                    title: video.title,
+                    url: url
                 }
+
+                console.log(song)
 
                 if (serverQue) {
                     serverQue.songs.push(song)
@@ -48,7 +51,7 @@ export default class Play extends Command {
                 
                 const queueConstruct = {
                     tc: msg.channel,
-                    vc: msg.member.voice.channel,
+                    vc: channel,
                     connection: null,
                     songs: [],
                     volume: 2,
@@ -57,6 +60,41 @@ export default class Play extends Command {
 
                 msg.client.queue.set(msg.guild.id, queueConstruct)
                 queueConstruct.songs.push(song)
+
+                const play = async song => {
+                    const queue = msg.client.queue.get(msg.guild.id)
+
+                    if (!song) {
+                        queue.vc.leave()
+                        msg.client.queue.delete(msg.guild.id)
+                        return
+                    }
+
+                    const dispatcher = queue.connection.play(ytdl(song.url))
+                        .on('finish', () => {
+                            queue.songs.shift()
+                            play(queue.songs[0])
+                        })
+                        .on('error', error => console.log(error))
+
+                    dispatcher.setVolumeLogarithmic(queue.volume / 5)
+                    queue.tc.send(`Started playing: **${song.title}**`)
+                }
+
+                try {
+                    const connection = await channel.join();
+                    queueConstruct.connection = connection
+                    play(queueConstruct.songs[0])
+                } catch (error) {
+                    console.log(`Could not join the voice channel: ${error}`)
+                    msg.client.queue.delete(msg.guild.id)
+                    await channel.leave()
+                    return msg.channel.send(`Could not join the voice channel: ${error}`)
+                }
+            })
+            .catch(err => {
+                console.log(err)
+                return msg.channel.send(`Something went wrong!`)
             })
     }
 }
