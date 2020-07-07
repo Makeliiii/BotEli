fc20a31d345e08a7f21e0d889677813f7ccf4eab
Joel <joel.m.maenpaa@gmail.com>
2020-07-08 01:40:20 +0300

    removed discord player

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import LeninardoClient from './client/Leninardo'
-import { Player } from 'discord-player'
 import dotenv from 'dotenv'
 
 dotenv.config()
 const ownerID = process.env.ownerID
 const token = process.env.TOKEN
-const youtube = process.env.youtube
 
 const client = new LeninardoClient(ownerID, token)
-
-export const player = new Player(client, youtube)
 
 client.start()
