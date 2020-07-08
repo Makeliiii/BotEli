eaecaa7ae4c92f298157a03622b83ccc258c4622
Joel <joel.m.maenpaa@gmail.com>
2020-07-09 01:34:09 +0300

    big spaghetti but it worksgit add .!

--- src/commands/music/Play.js
+++ src/commands/music/Play.js
@@ -35,6 +35,14 @@ export default class Play extends Command {
                 const serverQue = msg.client.queue.get(msg.guild.id)
                 let song = {}
                 let url
+                const queueConstruct = {
+                    tc: msg.channel,
+                    vc: channel,
+                    connection: null,
+                    songs: [],
+                    volume: 2,
+                    playing: true
+                }
 
                 if (Array.isArray(video)) {
                     const responses = [1, 2, 3, 4, 5]
@@ -45,9 +53,51 @@ export default class Play extends Command {
                     msg.channel.send(`**The search turned up these songs/videos:** \n\n1. **${video[0].title}**\n2. **${video[1].title}**\n3. **${video[2].title}**\n4. **${video[3].title}**\n5. **${video[4].title}**\n\n**Type the number of the song you'd like to be played.**`)
                         .then(() => {
                             msg.channel.awaitMessages(filter, { max: 1, time: 15000, errors: ['time'] })
-                                .then(collected => {
-                                    console.log(collected)
-                                    msg.channel.send(`${collected.first()}`)
+                                .then(async collected => {
+                                    song = {
+                                        id: video[collected.first().content - 1].id,
+                                        title: video[collected.first().content - 1].title,
+                                        url: video[collected.first().content - 1].url
+                                    }
+
+                                    if (serverQue) {
+                                        serverQue.songs.push(song)
+                                        console.log(serverQue.songs)
+                                        return msg.channel.send(`**${song.title}** has been added to the queue!`)
+                                    }
+
+                                    msg.client.queue.set(msg.guild.id, queueConstruct)
+                                    queueConstruct.songs.push(song)
+
+                                    const play = async song => {
+                                        const queue = msg.client.queue.get(msg.guild.id)
+                        
+                                        if (!song) {
+                                            queue.vc.leave()
+                                            msg.client.queue.delete(msg.guild.id)
+                                            return
+                                        }
+                        
+                                        const dispatcher = queue.connection.play(ytdl(song.url), { volume: 0.2 })
+                                            .on('finish', () => {
+                                                queue.songs.shift()
+                                                play(queue.songs[0])
+                                            })
+                                            .on('error', error => console.log(error))
+                        
+                                        msg.channel.send(`Started playing: **${song.title}**`)
+                                    }
+                        
+                                    try {
+                                        const connection = await channel.join();
+                                        queueConstruct.connection = connection
+                                        play(queueConstruct.songs[0])
+                                    } catch (error) {
+                                        console.log(`Could not join the voice channel: ${error}`)
+                                        msg.client.queue.delete(msg.guild.id)
+                                        await channel.leave()
+                                        return msg.channel.send(`Could not join the voice channel: ${error}`)
+                                    }
                                 })
                                 .catch(collected => {
                                     msg.channel.send(`Why no answer pepeHands...`)
@@ -61,59 +111,50 @@ export default class Play extends Command {
                         title: video.title,
                         url: url
                     }
-                }
 
-                if (serverQue) {
-                    serverQue.songs.push(song)
-                    console.log(serverQue.songs)
-                    return msg.channel.send(`**${song.title}** has been added to the queue!`)
-                }
-                    
-                const queueConstruct = {
-                    tc: msg.channel,
-                    vc: channel,
-                    connection: null,
-                    songs: [],
-                    volume: 2,
-                    playing: true
-                }
-    
-                msg.client.queue.set(msg.guild.id, queueConstruct)
-                queueConstruct.songs.push(song)
-    
-                const play = async song => {
-                    const queue = msg.client.queue.get(msg.guild.id)
-    
-                    if (!song) {
-                        queue.vc.leave()
-                        msg.client.queue.delete(msg.guild.id)
-                        return
+                    if (serverQue) {
+                        serverQue.songs.push(song)
+                        console.log(serverQue.songs)
+                        return msg.channel.send(`**${song.title}** has been added to the queue!`)
                     }
-    
-                    const dispatcher = await queue.connection.play(ytdl(song.url), { volume: 0.2 })
-                        .on('finish', () => {
-                            queue.songs.shift()
-                            play(queue.songs[0])
-                        })
-                        .on('error', error => console.log(error))
-    
-                    msg.channel.send(`Started playing: **${song.title}**`)
-                }
-    
-                try {
-                    const connection = await channel.join();
-                    queueConstruct.connection = connection
-                    play(queueConstruct.songs[0])
-                } catch (error) {
-                    console.log(`Could not join the voice channel: ${error}`)
-                    msg.client.queue.delete(msg.guild.id)
-                    await channel.leave()
-                    return msg.channel.send(`Could not join the voice channel: ${error}`)
+
+                    msg.client.queue.set(msg.guild.id, queueConstruct)
+                    queueConstruct.songs.push(song)
+
+                    const play = async song => {
+                        const queue = msg.client.queue.get(msg.guild.id)
+        
+                        if (!song) {
+                            queue.vc.leave()
+                            msg.client.queue.delete(msg.guild.id)
+                            return
+                        }
+        
+                        const dispatcher = queue.connection.play(ytdl(song.url), { volume: 0.2 })
+                            .on('finish', () => {
+                                queue.songs.shift()
+                                play(queue.songs[0])
+                            })
+                            .on('error', error => console.log(error))
+        
+                        msg.channel.send(`Started playing: **${song.title}**`)
+                    }
+        
+                    try {
+                        const connection = await channel.join();
+                        queueConstruct.connection = connection
+                        play(queueConstruct.songs[0])
+                    } catch (error) {
+                        console.log(`Could not join the voice channel: ${error}`)
+                        msg.client.queue.delete(msg.guild.id)
+                        await channel.leave()
+                        return msg.channel.send(`Could not join the voice channel: ${error}`)
+                    }
                 }
             })
             .catch(err => {
                 console.log(err)
-                return msg.channel.send(`Error: ${err}`)
+                return msg.channel.send(`${err}`)
             })
     }
 }
