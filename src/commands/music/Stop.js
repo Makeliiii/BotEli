54d9b384cf7636558763ca525548c5115ac960dd
Joel <joel.m.maenpaa@gmail.com>
2020-07-10 02:12:20 +0300

    stop command

--- src/commands/music/Stop.js
+++ src/commands/music/Stop.js
@@ -1,5 +1,4 @@
 import { Command } from 'discord-akairo'
-import { player } from '../../index'
 
 export default class Stop extends Command {
     constructor() {
@@ -7,17 +6,18 @@ export default class Stop extends Command {
             aliases: ['stop', 'lopeta'],
             ratelimit: 1,
             category: 'music',
-            description: 'Stop playing the current song.',
+            description: 'Stop playing songs.',
         })
     }
 
-    async exec(msg) {
-        await player.stop(msg.guild.id)
-            .then(song => {
-                return msg.channel.send(`Song ${song.name} stopped!`)
-            })
-            /*.catch(() => {
-                return msg.channel.send('Something went wrong!')
-            })*/
+    exec(msg) {
+        const { channel } = msg.member.voice
+        const serverQue = msg.client.queue.get(msg.guild.id)
+
+        if (!channel) return msg.channel.send('You must be in a voice channel to play music!')
+        if (!serverQue) return msg.channel.send('There is nothing playing...')
+
+        serverQue.songs = []
+        serverQue.connection.dispatcher.end('Stop has been used!')
     }
 }
